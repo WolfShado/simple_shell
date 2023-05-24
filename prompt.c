09d4ef0101467f6dd2d10c194a440a6f8586c6c0
Chamesseddine Souib <chamess.souib@gmail.com>
2023-05-24 02:56:59 -0700

    setting std

---
+++ prompt.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+
+/**
+ * get_path -  CMD's full path function finder.
+ * @cmd: cmd's ptr.
+ * Return: Ptr to the full path of the cmd,
+		or NULL if not found
+ */
+char *get_path(char *cmd)
+{
+	char *path = get_environment_variable("PATH");
+	char *token, *path_copy;
+	char *full_path = NULL;
+
+path_copy = strdup(path);
+if (path_copy == NULL)
+{
+perror("Failed to allocate memory");
+return (NULL);
+}
+token = strtok(path_copy, ":");
+while (token != NULL)
+{
+full_path = malloc(strlen(token) + strlen(cmd) + 2);
+if (full_path == NULL)
+{
+perror("Failed to allocate memory");
+free(path_copy);
+return (NULL);
+}
+sprintf(full_path, "%s/%s", token, cmd);
+if (access(full_path, X_OK) == 0)
+break;
+free(full_path);
+full_path = NULL;
+token = strtok(NULL, ":");
+}
+free(path_copy);
+return (full_path);
+}
